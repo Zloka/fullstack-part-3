0beeb2ea4e70577aff0625c1a496ab5007ed0e4e
Zacharias Levander <zacharias.levander@mafy.fi>
2020-12-12 12:07:26 +0200

    Switch over to displaying persons
    This marks my answer for exercise 3.1.

--- index.js
+++ index.js
@@ -2,24 +2,26 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-let notes = [
+let persons = [
   {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
   },
   {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
   },
   {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
   }
 ]
 
@@ -27,36 +29,36 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
+  const person = persons.find(person => person.id === id)
   
-  if (note) {
-    response.json(note)
+  if (person) {
+    response.json(person)
   } else {
     response.status(404).end()
   }
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
 })
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
     : 0
   return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/persons', (request, response) => {
   const { body } = request;
   if (!body.content) {
     return response.status(400).json({ 
@@ -64,16 +66,16 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const person = {
     content: body.content,
     important: body.important || false,
     date: new Date(),
     id: generateId(),
   }
 
-  notes = notes.concat(note)
+  persons = persons.concat(person)
 
-  response.json(note)
+  response.json(person)
 })
 
 const PORT = 3001
